4ee686da6384a3ca3f6b3b6579dec6b82ed3bfc3
Luiz Henrique <lhenrique78155@gmail.com>
2023-07-27 20:30:52 -0300

    criação dos codigos relacionados ao calendario, botoes e escutadores do calendário

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const monthText = document.querySelector('#monthText');
 const todayBtn = document.querySelector('#today');
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
+const days = document.querySelector('.days');
 
 const months = [
   "January",
@@ -24,7 +25,7 @@ const nameDays = document.querySelector('.nameDays');
 for (let index = 0; index < weekDays.length; index += 1) {
   const dayIndex = weekDays[index];
   const day = document.createElement('li');
-  day.classList = 'dayName list-none border-2 border-solid border-blue-300 w-[70px] ml-1 text-center';
+  day.classList = 'dayName list-none border-2 border-solid border-blue-300 w-[45px] ml-1 text-center';
   day.innerHTML = dayIndex;
   nameDays.appendChild(day);
 }
@@ -57,12 +58,11 @@ const generateCalendar = () => {
   
   // Cria os dias do mes anterior que está no calendário
   
-  const days = document.querySelector('.days')
   
   for (let indexDay = firstDay.getDay(); indexDay > 0; indexDay -= 1) {
     const numberDay = document.createElement('li');
     numberDay.innerHTML = '';
-    numberDay.classList = 'day-previous w-[70px] h-[70px] text-slate-400 text-center align-center';
+    numberDay.classList = 'day-previous w-[45px] h-[45px] text-slate-400 text-center align-center';
     numberDay.innerHTML = `${lastDayPreviousMonthDate - indexDay + 1}`;
     days.appendChild(numberDay)
   }
@@ -74,13 +74,13 @@ const generateCalendar = () => {
     const numberDay = document.createElement('li');
     numberDay.innerHTML = '';
     if (indexD === new Date().getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear()) {
-      numberDay.classList = 'day-actual w-[70px] h-[70px] text-center align-center';
+      numberDay.classList = 'day-actual w-[45px] h-[45px] text-center align-center bg-blue-100';
       numberDay.innerHTML = `${indexD}`;
       days.appendChild(numberDay);
     } else {
       // const numberDay = document.createElement('li');
       // numberDay.innerHTML = '';
-      numberDay.classList = 'day w-[70px] h-[70px] text-center align-center';
+      numberDay.classList = 'day w-[45px] h-[45px] text-center align-center';
       numberDay.innerHTML = `${indexD}`;
       days.appendChild(numberDay);
     }
@@ -90,9 +90,63 @@ const generateCalendar = () => {
   for (let indexNext = 1; indexNext <= startDayNextMonth; indexNext += 1) {
     const numberDay = document.createElement('li');
     numberDay.innerHTML = '';
-    numberDay.classList = 'next-month-day w-[70px] h-[70px] text-slate-400 text-center align-center';
+    numberDay.classList = 'next-month-day w-[45px] h-[45px] text-slate-400 text-center align-center';
     numberDay.innerHTML = `${indexNext}`;
     days.appendChild(numberDay);
   }
+  hideTodayButton();
 }
 generateCalendar()
+
+// Botao do mes seguinte
+
+nextBtn.addEventListener('click', () => {
+  days.innerHTML = '';
+  currentMonth +=1;
+  if (currentMonth > 11) {
+    currentMonth = 0;
+    currentYear += 1;
+  }
+  
+  generateCalendar();
+});
+
+// Botao do mes anterior
+
+prevBtn.addEventListener('click', () => {
+  days.innerHTML = '';
+  currentMonth -=1;
+  if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear -= 1;
+  }
+  generateCalendar();
+})
+
+days.addEventListener('mouseover', (event) => {
+  const focusDay = event.target;
+  focusDay.classList.add('bg-blue-100');
+})
+
+days.addEventListener('mouseout', (event) => {
+  const focusDay = event.target;
+  focusDay.classList.remove('bg-blue-100')
+})
+
+// Botao do dia atual
+
+todayBtn.addEventListener('click', () => {
+  days.innerHTML = '';
+  currentMonth = date.getMonth();
+  currentYear = date.getFullYear();
+  generateCalendar();
+})
+
+function hideTodayButton() {
+  if (currentMonth == new Date().getMonth() && currentYear == new Date().getFullYear()) {
+    todayBtn.style.display = 'none';
+  } else {
+    todayBtn.style.display = 'inline-block';
+  }
+}
+
